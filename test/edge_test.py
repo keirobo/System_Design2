e636863f2c43f002c46f3e40bfcdcb2ae1a8e8ca
keirobo <61536632+keirobo@users.noreply.github.com>
2022-10-24 14:15:03 +0900

    エッジ検出ができた。canny法が良さげ

---
+++ test/edge_test.py
@@ -0,0 +1,14 @@
+import cv2
+
+#グレースケールで読み込み
+gray_img = cv2.imread("test.jpg", 0)
+
+#エッジ検出
+sobel_img = cv2.Sobel(gray_img, cv2.CV_32F, 1, 1, 1, 5) #sobel
+laplacian_img = cv2.Laplacian(gray_img, cv2.CV_32F, 1, 5) #laplacian
+canny_img = cv2.Canny(gray_img, 50, 150) #canny
+
+#書き込み
+# cv2.imwrite("sobel.jpg", sobel_img)
+# cv2.imwrite("laplacian.jpg", laplacian_img)
+cv2.imwrite("canny.jpg", canny_img)
