0674ff6e72573ccdd9ce1b508821c3dc4ab8ecce
keirobo <61536632+keirobo@users.noreply.github.com>
2022-10-28 09:49:58 +0900

    工具部分だけ切り取ることに成功

--- test/comparison_test.py
+++ test/comparison_test.py
@@ -3,30 +3,9 @@ import numpy as np
 import random
 import sys
 
+def estimation(img):
+    canny_img = cv2.Canny(img, 50, 150) #canny
 
-if __name__ == '__main__':
-    #グレースケールで読み込み
-    initial_img = cv2.imread("initial.jpg", 0)
-
-    # 写真撮るとき ============================
-    # deviceid=0 # it depends on the order of USB connection. 
-    # capture = cv2.VideoCapture(deviceid)
-
-    # ret, gray_img = capture.read()
-
-    #==========================================
-
-    gray_img = cv2.imread("data1.jpg", 0)
-    
-    #エッジ検出
-    sobel_img = cv2.Sobel(gray_img, cv2.CV_32F, 1, 1, 1, 5) #sobel
-    laplacian_img = cv2.Laplacian(gray_img, cv2.CV_32F, 1, 5) #laplacian
-    canny_img = cv2.Canny(gray_img, 50, 150) #canny
-
-    # 画像をグレースケールで読み込み
-    # gray_src = cv2.imread(canny_img, 0)
-    # canny_img
-    # 前処理（平準化フィルターを適用した場合）
     # 前処理が不要な場合は下記行をコメントアウト
     blur_src = cv2.GaussianBlur(canny_img, (5, 5), 2)
     # 二値変換
@@ -44,6 +23,7 @@ if __name__ == '__main__':
     tmp_center = np.delete(label[3], 0, 0)
     data = []
     center = []
+    tool = []
     j = 0
     # オブジェクト情報を利用してラベリング結果を画面に表示
     for i in range(n):
@@ -58,14 +38,43 @@ if __name__ == '__main__':
         y0 = data[i][1]
         x1 = data[i][0] + data[i][2]
         y1 = data[i][1] + data[i][3]
+        
+        # 画像の工具部分の切り出し
+        tool.append(color_src01[y0 : y1, x0: x1])
+        # cv2.imwrite("out_sample1.jpg", tool[i])
+        
+        # 工具を囲う四角とか座標とかを表示したいとき
+        # cv2.rectangle(color_src01, (x0, y0), (x1, y1), (0, 0, 255))
+        # # # 各オブジェクトのラベル番号と面積に黄文字で表示
+        # # cv2.putText(color_src01, "ID: " +str(i + 1), (x1 - 20, y1 + 15), cv2.FONT_HERSHEY_PLAIN, 1, (0, 255, 255))
+        # cv2.putText(color_src01, "S: " +str(data[i][4]), (x1 - 20, y1 + 15), cv2.FONT_HERSHEY_PLAIN, 1, (0, 255, 255))
+        # # # 各オブジェクトの重心座標を黄文字で表示
+        # cv2.putText(color_src01, "X: " + str(int(center[i][0])), (x1 - 20, y1 + 30), cv2.FONT_HERSHEY_PLAIN, 1, (0, 255, 255))
+        # cv2.putText(color_src01, "Y: " + str(int(center[i][1])), (x1 - 20, y1 + 45), cv2.FONT_HERSHEY_PLAIN, 1, (0, 255, 255))
 
-        cv2.rectangle(color_src01, (x0, y0), (x1, y1), (0, 0, 255))
-        # # 各オブジェクトのラベル番号と面積に黄文字で表示
-        # cv2.putText(color_src01, "ID: " +str(i + 1), (x1 - 20, y1 + 15), cv2.FONT_HERSHEY_PLAIN, 1, (0, 255, 255))
-        cv2.putText(color_src01, "S: " +str(data[i][4]), (x1 - 20, y1 + 15), cv2.FONT_HERSHEY_PLAIN, 1, (0, 255, 255))
-        # # 各オブジェクトの重心座標を黄文字で表示
-        cv2.putText(color_src01, "X: " + str(int(center[i][0])), (x1 - 20, y1 + 30), cv2.FONT_HERSHEY_PLAIN, 1, (0, 255, 255))
-        cv2.putText(color_src01, "Y: " + str(int(center[i][1])), (x1 - 20, y1 + 45), cv2.FONT_HERSHEY_PLAIN, 1, (0, 255, 255))
+    return color_src01, tool, center
+
+
+if __name__ == '__main__':
+    #グレースケールで読み込み
+    initial = cv2.imread("initial.jpg", 0)
+
+    init_img, init_tool, init_center = estimation(initial)
+    
+    # cv2.imwrite("test_img0.jpg", init_img)
+    # cv2.imwrite("test_img1.jpg", init_tool[0])
+    # cv2.imwrite("test_img2.jpg", init_tool[1])
+
+
+    # 写真撮るとき ============================
+    # deviceid=0 # it depends on the order of USB connection. 
+    # capture = cv2.VideoCapture(deviceid)
+
+    # ret, gray_img = capture.read()
+
+    #==========================================
+
+    gray_img = cv2.imread("data1.jpg", 0)
 
     # 結果の表示
-    cv2.imwrite("test_img.jpg", color_src01)
+    # cv2.imwrite("test_img.jpg", estimation(gray_img))
