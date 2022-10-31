294d21834d517c2a66aaa4c744aed33d4e6d6cc9
keirobo <61536632+keirobo@users.noreply.github.com>
2022-10-31 15:48:54 +0900

    中を埋める機能の実装

--- test/comparison_test.py
+++ test/comparison_test.py
@@ -5,18 +5,42 @@ import sys
 
 def img_processing(img):
     #エッジ検出
-    canny_img = cv2.Canny(img, 50, 150) #canny
-
+    canny_img = cv2.Canny(img, 60, 141) #canny
+    # 画像をグレースケールで読み込み
+    
+    # 前処理（平準化フィルターを適用した場合）
     # 前処理が不要な場合は下記行をコメントアウト
-    blur_src = cv2.GaussianBlur(canny_img, (5, 5), 2)
+    blur_src = cv2.GaussianBlur(canny_img, (5, 5), 3)
     # 二値変換
     # 前処理を使用しなかった場合は、blur_srcではなくgray_srcに書き換えるする
     mono_src = cv2.threshold(blur_src, 48, 255, cv2.THRESH_BINARY_INV)[1]
-    mono_src = 255 - mono_src
     # ラベリング結果書き出し用に二値画像をカラー変換
-    color_src01 = cv2.cvtColor(mono_src, cv2.COLOR_GRAY2BGR)
-    # ラベリング処理
-    label = cv2.connectedComponentsWithStats(mono_src)
+    
+    th, src_th = cv2.threshold(mono_src, 220, 255, cv2.THRESH_BINARY_INV)
+     
+    # Copy the thresholded image.
+    im_floodfill = src_th.copy()
+     
+    # Mask used to flood filling.
+    # Notice the size needs to be 2 pixels than the image.
+    h, w = src_th.shape[:2]
+    mask = np.zeros((h+2, w+2), np.uint8)
+     
+    # Floodfill from point (0, 0)
+    cv2.floodFill(im_floodfill, mask, (10,10), 255)
+     
+    # Invert floodfilled image
+    im_floodfill_inv = cv2.bitwise_not(im_floodfill)
+     
+    # Combine the two images to get the foreground.
+    src_out = src_th | im_floodfill_inv
+    
+    color_src01 = cv2.cvtColor(src_out, cv2.COLOR_GRAY2BGR)
+    # color_src02 = cv2.cvtColor(src_out, cv2.COLOR_GRAY2BGR)
+    
+    # cv2.imwrite("data_initial.jpg", color_src02)
+    
+    label = cv2.connectedComponentsWithStats(src_out)
     # オブジェクト情報を項目別に抽出
     n = label[0] - 1
     tmp_data = np.delete(label[2], 0, 0)
@@ -38,12 +62,9 @@ def img_processing(img):
         y0 = data[i][1]
         x1 = data[i][0] + data[i][2]
         y1 = data[i][1] + data[i][3]
-        
-        # 画像の工具部分の切り出し
+      
         tool.append(color_src01[y0 : y1, x0: x1])
-        # cv2.imwrite("out_sample1.jpg", tool[i])
-        
-        # 工具を囲う四角とか座標とかを表示したいとき
+      
         # cv2.rectangle(color_src01, (x0, y0), (x1, y1), (0, 0, 255))
         # # # 各オブジェクトのラベル番号と面積に黄文字で表示
         # # cv2.putText(color_src01, "ID: " +str(i + 1), (x1 - 20, y1 + 15), cv2.FONT_HERSHEY_PLAIN, 1, (0, 255, 255))
@@ -68,9 +89,9 @@ if __name__ == '__main__':
 
     init_img, init_tool, init_center = img_processing(initial)
     
-    # cv2.imwrite("test_img0.jpg", init_img)
-    # cv2.imwrite("test_img1.jpg", init_tool[0])
-    # cv2.imwrite("test_img2.jpg", init_tool[1])
+    cv2.imwrite("test_img00.jpg", init_img)
+    cv2.imwrite("test_img01.jpg", init_tool[0])
+    cv2.imwrite("test_img02.jpg", init_tool[1])
 
 
     # 写真撮るとき ============================
@@ -84,6 +105,10 @@ if __name__ == '__main__':
     gray_img = cv2.imread("data1.jpg", 0)
 
     now_img, now_tool, now_center = img_processing(gray_img)
+
+    cv2.imwrite("test_img10.jpg", now_img)
+    cv2.imwrite("test_img11.jpg", now_tool[0])
+    cv2.imwrite("test_img12.jpg", now_tool[1])
 
     if len(now_tool) > len(init_tool): num = len(now_tool)
     else: num = len(init_tool)
