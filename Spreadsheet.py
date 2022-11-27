1c305ab97431f2c4af6d8eff7067228bebcca3fd
keirobo <keirobo@example.com>
2022-11-27 20:34:23 +0900

    こっちのほうがよさそう

--- Spreadsheet.py
+++ Spreadsheet.py
@@ -1,9 +1,24 @@
 import gspread
+from google.oauth2.service_account import Credentials
+# お決まりの文句
+# 2つのAPIを記述しないとリフレッシュトークンを3600秒毎に発行し続けなければならない
+scope = ['https://www.googleapis.com/auth/spreadsheets','https://www.googleapis.com/auth/drive']
+#ダウンロードしたjsonファイル名をクレデンシャル変数に設定。
+credentials = Credentials.from_service_account_file("data.json", scopes=scope)
+#OAuth2の資格情報を使用してGoogle APIにログイン。
+gc = gspread.authorize(credentials)
+#スプレッドシートIDを変数に格納する。
+SPREADSHEET_KEY = '1TbHWzz_DXu8TPnwO-rwzJTcQct0bKgsgQmNFsuwz0Fs'
+# スプレッドシート（ブック）を開く
+workbook = gc.open_by_key(SPREADSHEET_KEY)
 
-key_name = 'data.json'
-sheet_name = 'データ'
+# シートの一覧を取得する。（リスト形式）
+worksheets = workbook.worksheets()
+print(worksheets)
+# シートを開く
+worksheet = workbook.worksheet('シート1')
+# セルA1に”test value”という文字列を代入する。
+worksheet.update_cell(1, 1, 'test value')
 
-gc = gspread.service_account(filename = key_name)
-
-wks = gc.open(sheet_name).sheet1
-wks.update_cell(1, 1, 'Pythonから入力')
+data = worksheet.find("aa")
+print(data)
