9d3223ef21af9333faccaa35a24093ba1759e212
keirobo <61536632+keirobo@users.noreply.github.com>
2022-12-16 09:43:19 +0900

    いらないやつ削除

--- Spreadsheet.py
+++ Spreadsheet.py
@@ -3,29 +3,6 @@ from google.oauth2.service_account import Credentials
 import re
 import wrapt_timeout_decorator
 
-# # お決まりの文句
-# # 2つのAPIを記述しないとリフレッシュトークンを3600秒毎に発行し続けなければならない
-# scope = ['https://www.googleapis.com/auth/spreadsheets','https://www.googleapis.com/auth/drive']
-# #ダウンロードしたjsonファイル名をクレデンシャル変数に設定。
-# credentials = Credentials.from_service_account_file("data.json", scopes=scope)
-# #OAuth2の資格情報を使用してGoogle APIにログイン。
-# gc = gspread.authorize(credentials)
-# #スプレッドシートIDを変数に格納する。
-# SPREADSHEET_KEY = '1TbHWzz_DXu8TPnwO-rwzJTcQct0bKgsgQmNFsuwz0Fs'
-# # スプレッドシート（ブック）を開く
-# workbook = gc.open_by_key(SPREADSHEET_KEY)
-
-# # シートの一覧を取得する。（リスト形式）
-# worksheets = workbook.worksheets()
-# print(worksheets)
-# # シートを開く
-# worksheet = workbook.worksheet('シート1')
-# # セルA1に”test value”という文字列を代入する。
-# worksheet.update_cell(1, 1, 'test value')
-
-# data = worksheet.find("aa")
-# print(data)
-
 @wrapt_timeout_decorator.timeout(dec_timeout=10)
 def init(key):
   global KEY, gc, workbook
