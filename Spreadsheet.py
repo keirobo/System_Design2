31b6a229cc10216665b561e63754ecdd2241b223
keirobo <keirobo@example.com>
2022-12-17 23:55:24 +0900

    タイムアウト処理を使うとグローバル変数周りがおかしくなるっぽい

--- Spreadsheet.py
+++ Spreadsheet.py
@@ -3,9 +3,9 @@ from google.oauth2.service_account import Credentials
 import re
 import wrapt_timeout_decorator
 
-@wrapt_timeout_decorator.timeout(dec_timeout=10)
+# @wrapt_timeout_decorator.timeout(dec_timeout=10)
 def init(key):
-  global KEY, gc, workbook
+  global workbook
   # お決まりの文句
   # 2つのAPIを記述しないとリフレッシュトークンを3600秒毎に発行し続けなければならない
   scope = ['https://www.googleapis.com/auth/spreadsheets','https://www.googleapis.com/auth/drive']
@@ -17,9 +17,14 @@ def init(key):
   KEY = key
   workbook = gc.open_by_key(KEY)
 
-def get_user_id(id):
+  print(workbook)
+
+  return workbook
+
+
+def get_slack_id(id):
   # シートを開く
-  worksheet = workbook.worksheet('ユーザー情報')
+  worksheet = workbook.worksheet('Slack')
 
   tmp1 = worksheet.find(str(id))
   tmp2 = worksheet.find("Slack_ID")
@@ -32,3 +37,13 @@ def get_user_id(id):
   print(str(tmp1.row) + " " + str(tmp2.col))
 
   return worksheet.cell(tmp1.row, tmp2.col).value
+
+
+def get_max_id():
+  worksheet = workbook.worksheet('ユーザー情報')
+  
+  tmp = worksheet.find("ユーザーID")
+  
+  data = worksheet.col_values(tmp.col)
+
+  print(data)
