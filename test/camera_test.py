a43dd65a5739f50cf24cc862babcb1dcb2cdd219
keirobo <x99deluxe913@gmail.com>
2023-01-05 21:54:28 +0900

    テストファイルの削除

--- test/camera_test.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-
-import cv2
-
-deviceid=0 # it depends on the order of USB connection. 
-capture = cv2.VideoCapture(deviceid)
-
-ret, frame = capture.read()
-cv2.imwrite('camera_test.jpg', frame)
